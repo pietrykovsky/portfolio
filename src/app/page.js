d5a386efe58ae7fdb4547e283fbd075d265a657d
pietrykovsky <m.pietrykowski2001@gmail.com>
2024-06-30 19:53:32 +0200

    add header section with animation and typewriter effect, add second section placeholder

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,56 @@
 "use client";
-
 import styles from "./page.module.css";
-import Container from "react-bootstrap/Container";
+import { Container, Row, Col, Image } from "react-bootstrap";
+import Typewriter from "typewriter-effect";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Home</h1>
-      <p>
-        Hello world!
-      </p>
-    </main>
+    <>
+      <Container fluid className={styles.tildeBackground}>
+        <Container className="my-5">
+          <Row>
+            <Col className="align-content-center p-3" md={7}>
+              <h1>
+                <Typewriter
+                  onInit={(typewriter) => {
+                    typewriter
+                      .typeString("Hello world!<br>I'm ")
+                      .typeString(`<span class="${styles.highlighted}">Michał Pietrykowski</span>`)
+                      .typeString(" -<br>a <b>Software Developer</b>")
+                      .start();
+                  }}
+                  options={{delay: 50}}
+                />
+              </h1>
+            </Col>
+            <Col className="p-3" md={5}>
+              <Image 
+                className="img-fluid" 
+                src="/assets/home-img.svg" 
+                alt="Home Image" 
+                style={{ maxHeight: "500px" }}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+      <Container className="my-5">
+        <Row>
+          <Col className="text-center p-3" md={5}>
+            <Image 
+              src="/assets/avatar.jpg" 
+              alt="My Photo" 
+              style={{ maxHeight: "250px" }}
+              roundedCircle
+            />
+          </Col>
+          <Col className="align-content-center p-3" md={7}>
+            <h1>
+              placeholder text
+            </h1>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
