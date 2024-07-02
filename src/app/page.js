832fd06f3ffcdef22b85c2d34dd821379e48c20a
Michał Pietrykowski <93202530+pietrykovsky@users.noreply.github.com>
2024-07-02 13:33:35 +0200

    Automate deployment process (#2)
    * add node ci workflow
    
    * revert eslintrc config change
    
    * fix linting issues
    
    * add deploy config
    
    * update known_hosts
    
    * remove debug

--- src/app/page.js
+++ src/app/page.js
@@ -48,13 +48,13 @@ export default function Home() {
           <Col className="align-content-center p-3" md={7}>
             <h2 className="mb-4">Welcome to <span className={styles.highlighted}>My Digital Universe</span></h2>
             <p>
-              I'm a 23-year-old code enthusiast from Wrocław, currently navigating the galaxy of IT Automation Systems at Wrocław University of Science and Technology. My journey from <span className={styles.highlighted}>QBASIC</span> text adventures to full-stack engineering has been quite the space odyssey!
+              I&apos;m a 23-year-old code enthusiast from Wrocław, currently navigating the galaxy of IT Automation Systems at Wrocław University of Science and Technology. My journey from <span className={styles.highlighted}>QBASIC</span> text adventures to full-stack engineering has been quite the space odyssey!
             </p>
             <p>
-              As a full-stack engineer, I wield the power of <span className={styles.highlighted}>Python</span> and <span className={styles.highlighted}>JavaScript</span>, crafting digital solutions with frameworks like <span className={styles.highlighted}>Django</span> and <span className={styles.highlighted}>React</span>. I've also ventured into the realm of <span className={styles.highlighted}>C#</span>, developing desktop applications that push the boundaries of what's possible.
+              As a full-stack engineer, I wield the power of <span className={styles.highlighted}>Python</span> and <span className={styles.highlighted}>JavaScript</span>, crafting digital solutions with frameworks like <span className={styles.highlighted}>Django</span> and <span className={styles.highlighted}>React</span>. I&apos;ve also ventured into the realm of <span className={styles.highlighted}>C#</span>, developing desktop applications that push the boundaries of what&apos;s possible.
             </p>
             <p>
-              When I'm not busy decoding the mysteries of the digital realm or experimenting with <span className={styles.highlighted}>machine learning</span> algorithms, you might find me lost in Asimov's worlds, mastering the art of Japanese tea ceremonies, or saving Night City in Cyberpunk 2077. And yes, I can debug code almost as fast as I can slurp a bowl of ramen - it's my superpower!
+              When I&apos;m not busy decoding the mysteries of the digital realm or experimenting with <span className={styles.highlighted}>machine learning</span> algorithms, you might find me lost in Asimov&apos;s worlds, mastering the art of Japanese tea ceremonies, or saving Night City in Cyberpunk 2077. And yes, I can debug code almost as fast as I can slurp a bowl of ramen - it&apos;s my superpower!
             </p>
           </Col>
         </Row>
