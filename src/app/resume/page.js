a560375d65b40e1f44c3a03f78f9856970924060
pietrykovsky <m.pietrykowski2001@gmail.com>
2024-07-03 23:36:45 +0200

    change pdf worker source

--- src/app/resume/page.js
+++ src/app/resume/page.js
@@ -8,10 +8,7 @@ import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 import styles from './page.module.css';
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
-  import.meta.url,
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 export default function Resume() {
   const pdfUrl = '/resume_preview.pdf';
